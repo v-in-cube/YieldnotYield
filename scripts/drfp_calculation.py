ebf920c62b431d20b574e9b1e066bccf8f54e158
Varvara Voinarovska <87934537+v-in-cube@users.noreply.github.com>
2023-09-19 17:19:46 +0200

    Update drfp_calculation.py

--- scripts/drfp_calculation.py
+++ scripts/drfp_calculation.py
@@ -1,13 +1,4 @@
-import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.svm import SVR
-from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-import seaborn as sns
-import json
 import joblib
 from drfp import DrfpEncoder
 
